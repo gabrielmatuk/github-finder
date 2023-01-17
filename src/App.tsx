a9a79adb693417aff46f8f763c4765a41e82526a
gabrielmatuk <gabrielmatuk14@gmail.com>
2023-01-17 20:42:43 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 const App = () => {
   return (
     <div className="App">
       <h1>GitHub Finder</h1>
+      <Outlet/>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//Pages
+import Home from "./routes/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
