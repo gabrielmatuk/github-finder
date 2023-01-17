9875f0bd38520bae3832a0fab6d2a7a8633f1369
gabrielmatuk <gabrielmatuk14@gmail.com>
2023-01-17 20:57:29 -0300

    feat: fetch on github api

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,23 @@
+import { UserProps } from "../types/user";
+import { GITHUB_URL } from "../constants/url";
+
 import Search from "../components/Search";
 
+import React from "react";
+
 const Home = () => {
+  const [user, setUser] = React.useState<UserProps | null>(null);
+    
+  const loadUser = async(userName: string) => {
+    const res = await fetch(`${GITHUB_URL}/${userName}`)
+    const data = await res.json();
+
+    console.log(data)
+  }
+
   return (
     <div>
-      <Search />
+      <Search loadUser={loadUser}/>
     </div>
   );
 };
