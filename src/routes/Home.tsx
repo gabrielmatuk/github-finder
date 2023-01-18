05942cf65c41c41f4cdbac945963f249deb90a34
gabrielmatuk <gabrielmatuk14@gmail.com>
2023-01-17 21:15:04 -0300

    feature user component

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,6 +2,7 @@ import { UserProps } from "../types/user";
 import { GITHUB_URL } from "../constants/url";
 
 import Search from "../components/Search";
+import User from "../components/User";
 
 import React from "react";
 
@@ -28,7 +29,7 @@ const Home = () => {
   return (
     <div>
       <Search loadUser={loadUser} />
-     {user && <p>{user.login}</p>}
+     {user && <User {...user}/>}
     </div>
   );
 };
